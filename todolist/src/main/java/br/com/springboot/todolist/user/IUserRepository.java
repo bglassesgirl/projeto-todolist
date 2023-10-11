19c0aa16df6945bcd4867898c0af389940e02ecf
bglassesgirl <luanabragagomes9@gmail.com>
2023-10-11 11:18:21 -0300

    crypt password

--- todolist/src/main/java/br/com/springboot/todolist/user/IUserRepository.java
+++ todolist/src/main/java/br/com/springboot/todolist/user/IUserRepository.java
@@ -3,9 +3,11 @@ package br.com.springboot.todolist.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.UUID;
 
 
 @Repository
 public interface IUserRepository extends JpaRepository<UserModel, UUID>{
     List<UserModel> findByUsername(String username);
+
 }
